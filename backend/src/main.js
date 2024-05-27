ed3d6a3f90ccf0e978d7455cd37661137124db61
Hikaru-Umezawa <hikaru.19970630@gmail.com>
2024-05-27 18:44:00 +0900

    add: vue-router

--- backend/src/main.js
+++ backend/src/main.js
@@ -1,7 +1,8 @@
 import { createApp } from "vue";
 import store from "./store";
+import router from "./router";
 import "./index.css";
 import "./style.css";
 import App from "./App.vue";
 
-createApp(App).use(store).mount("#app");
+createApp(App).use(store).use(router).mount("#app");
