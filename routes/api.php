22aa37eec1c7d0817b2787c07605fd087929cd5a
Hikaru-Umezawa <hikaru.19970630@gmail.com>
2024-06-08 13:02:32 +0900

    add: Admin middleware

--- routes/api.php
+++ routes/api.php
@@ -7,5 +7,11 @@ Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
 
+Route::middleware(['auth:sanctum', 'admin'])->group(function () {
+    Route::get('/user', function (Request $request) {
+        return $request->user();
+    });
+    Route::post('/logout', [\App\Http\Controllers\AuthController::class, 'logout']);
+});
+
 Route::post('/login', [\App\Http\Controllers\AuthController::class, 'login']);
-Route::post('/logout', [\App\Http\Controllers\AuthController::class, 'logout']);
